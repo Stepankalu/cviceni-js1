63d17a0b326013178e6fcfa7773ec0538f5d573d
Štěpánka <Doma@Stepanka-MacBook-Air.local>
2025-03-22 12:45:09 +0100

    cvičení js1

---
+++ script.js
@@ -0,0 +1,54 @@
+/* 
+const cisloNaKostce = Math.floor(Math.random() * 6) + 1;
+document.body.innerHTML += "Na kostce padlo: " + cisloNaKostce;
+ */
+
+const plat = 40000;
+const naklady = plat * 0.6;
+const danovaSazba = 0.15;
+const dan = (plat - naklady) * danovaSazba
+document.body.innerHTML +=  Math.floor(dan);
+
+
+/*  cvičení Výplata
+document.body.innerHTML += (7 * 320) * 21;
+document.body.innerHTML += "<br>";
+document.body.innerHTML += ((7 * 320) * 21) * 0.6;
+document.body.innerHTML += "<br>";
+document.body.innerHTML += (((7 * 320) * 21) * 0.6) * 0.15;
+document.body.innerHTML += "<br>"
+document.body.innerHTML += Math.round((((7 * 320) * 21) * 0.6) * 0.15)*/
+
+
+
+/*
+document.body.innerHTML += Math.round(12.333);
+document.body.innerHTML += "<br>";
+document.body.innerHTML += Math.floor(12.333);
+document.body.innerHTML += "<br>";
+document.body.innerHTML += Math.ceil(12.333);
+document.body.innerHTML += "<br>";
+document.body.innerHTML += Math.floor(Math.random() * 6) + 1;
+*/
+
+
+
+/*
+document.body.innerHTML += "Řekl: \"Ahoj světe!\"";
+document.body.innerHTML += "<br>";
+document.body.innerHTML += 42;
+document.body.innerHTML += "<br>";
+document.body.innerHTML += 2 + 5;
+document.body.innerHTML += "<br>";
+document.body.innerHTML += 2 - 5;
+document.body.innerHTML += "<br>";
+document.body.innerHTML += 2 * 5;
+document.body.innerHTML += "<br>";
+document.body.innerHTML += 12 / 2.5;
+document.body.innerHTML += "<br>";
+document.body.innerHTML += 2 + 3 * 4;
+document.body.innerHTML += "<br>";
+document.body.innerHTML += 27 % 4;
+*/
+
+//document.body.innerHTML += "Ahoj světe!" 
