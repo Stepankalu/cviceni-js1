37063824853c8f380b0db6c6ef27803801c6c0c0
Štěpánka <Doma@Stepanka-MacBook-Air.local>
2025-05-01 22:34:57 +0200

    dokončení cvičení a opakování JS1

--- script.js
+++ script.js
@@ -1,13 +1,84 @@
-/* 
+/* cvičení ultramaraton
+const start = 15;
+const delka = 15;
+const konec = (start + delka) % 24;
+document.body.innerHTML += "Čas konce v hodinách: " + konec;
+
+
+
+/* cvičení převod měny
+const wageInEur = 20;
+const kurzEuro = 24.55;
+const wageInCzk = wageInEur * kurzEuro;
+document.body.innerHTML += "<h1>Mzda v korunách" + ":" + " " + wageInCzk + " Kč</h1>"
+
+
+
+/*cvičení náhodné číslo
+document.body.innerHTML += Math.floor(Math.random() * 6) + 1;
+
+
+
+/*const plat = 40000;
+const naklady = plat * 0.6
+document.body.innerHTML += Math.floor((plat - naklady) * 0.15)
+
+
+
+//document.body.innerHTML += Math.round(12.333);
+
+/*cvičení email
+document.body.innerHTML += "Štěpánka" + "Lubovská" + "@mujemail.cz";
+
+
+/* cvičení délka filmu
+document.body.innerHTML += Math.floor(223/60);
+document.body.innerHTML += ":";
+document.body.innerHTML += 223 % 60
+
+/* document.body.innerHTML += "Ahoj světe!";
+document.body.innerHTML += "<br>";
+document.body.innerHTML += 42;
+document.body.innerHTML += "<br>";
+document.body.innerHTML += 2 + 5;
+document.body.innerHTML += "<br>";
+document.body.innerHTML += 2 - 5;
+document.body.innerHTML += "<br>";
+document.body.innerHTML += 2 * 5;
+document.body.innerHTML += "<br>";
+document.body.innerHTML += 12 / 4;
+document.body.innerHTML += "<br>";
+document.body.innerHTML += 12 / 5;
+
+/*const start = 15;
+const delka = prompt('Zadej délku závodu:');
+const konec = (start + delka) % 24;
+document.body.innerHTML += konec;
+
+
+
+
+/*const wageInCzk = 100
+
+document.body.innerHTML += '<h1>' + 'Mzda v hodinách ' + ',-' + '</h1>'
+
+
+
+
+
+
+/*
 const cisloNaKostce = Math.floor(Math.random() * 6) + 1;
 document.body.innerHTML += "Na kostce padlo: " + cisloNaKostce;
- */
+*/
 
-const plat = 40000;
+/*  const plat = 40000;
 const naklady = plat * 0.6;
 const danovaSazba = 0.15;
 const dan = (plat - naklady) * danovaSazba
 document.body.innerHTML +=  Math.floor(dan);
+*/
+
 
 
 /*  cvičení Výplata
